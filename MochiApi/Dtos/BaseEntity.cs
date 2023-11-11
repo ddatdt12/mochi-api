6b70d216ff5b5a2fdde1e92df582357e753697f7
Dat Do Thanh <ddatdt12@gmaill.com>
2023-11-11 23:34:21 +0700

    transaction api, verifycategory wallet api

---
+++ MochiApi/Dtos/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace MochiApi.Dtos
+{
+    public abstract class BaseEntityDto
+    {
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
